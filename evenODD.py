5efefa6f0ebe69012a7b26a9ace9ae1be45d6e79
Samad Azeez <117034555+Samad-Azeez@users.noreply.github.com>
2022-11-23 10:40:54 +0100

    EVEN or ODD number detector was created
    This helps in detecting if a number is even or odd by checking the reminder when it divides 2

---
+++ evenODD.py
@@ -0,0 +1,6 @@
+particular_Number = int(input("Enter the number you want to check :"))
+
+if particular_Number % 2 != 0:
+  print("THIS Number is an odd number")
+else:
+  print("This Number is an Even number")
